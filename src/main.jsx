d4488b71b50c94ef30b36a4252cca37db59cee90
leakrullp <leakrull96@gmail.com>
2025-10-23 15:01:48 +0200

    Import "tokens.css" only once inside of index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./components/tokens.css";
 import "./index.css";
 
 // Create the root element and render the App component inside StrictMode
