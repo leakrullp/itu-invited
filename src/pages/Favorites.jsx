ca7f6f65b7b3f6a094729057d44cb25c450d4959
thole <thole@itu.dk>
2025-10-28 22:22:48 +0100

    duplicate front page to Favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,3 +1,15 @@
+import { Filter, Navbar, Entry } from "../components";
+import data from "../assets/Data/Data";
+
 export const Favorites = () => {
-  return <h1>My favorited events</h1>;
+  // Map through data and create an Entry component for each data object
+  const cards = data.map((card) => <Entry key={card.img} {...card} />);
+
+  return (
+    <>
+      <main className="container">{cards}</main>
+
+      <Filter />
+    </>
+  );
 };
