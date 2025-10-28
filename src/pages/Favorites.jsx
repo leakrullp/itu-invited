aacac8d4e8164ee671d3e10c6c93f37636faadd5
thole <thole@itu.dk>
2025-10-28 23:09:16 +0100

    create DataFavorited a version of Data.js for Favorite Subpage

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,11 @@
 import { Filter, Navbar, Entry } from "../components";
-import data from "../assets/Data/Data";
+import dataFavorited from "../assets/Data/DataFavorited";
 
 export const Favorites = () => {
   // Map through data and create an Entry component for each data object
-  const cardsFavorited = data.map((card) => <Entry key={card.img} {...card} />);
+  const cardsFavorited = dataFavorited.map((card) => (
+    <Entry key={card.img} {...card} />
+  ));
 
   return (
     <>
