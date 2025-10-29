1964243f281e830029357b14f90d29ae393bc601
thole <thole@itu.dk>
2025-10-29 17:23:41 +0100

    mapping info from UserSettings datafile into User subpage

---
+++ src/assets/Data/UserSettings.js
@@ -0,0 +1,12 @@
+const UserSettings = [
+  {
+    id: 1,
+    name: "J.D. Vance",
+    role: "Admin",
+    picture: "src/assets/Data/profile_pic_JD_Vance.png",
+    organisation: "ITU_Underground",
+    mail: "JDV@itu.dk",
+  },
+];
+
+export default UserSettings;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,3 +1,33 @@
+import { Filter, Navbar, Entry, Button } from "../components";
+import UserSettings from "../assets/Data/UserSettings";
+
+const currentUserID = 1;
+
 export const User = () => {
-  return <h1>User settings</h1>;
+  // Find the user by ID
+  const currentUser = UserSettings.find((user) => user.id === currentUserID);
+
+  return (
+    <>
+      <h1>User settings</h1>
+      {
+        <div>
+          <h2>{currentUser.name}</h2>
+          <img
+            src={currentUser.picture}
+            alt={currentUser.name}
+            style={{
+              width: "100px",
+              height: "100px",
+              borderRadius: "50%",
+              objectFit: "cover",
+            }}
+          />
+          <p>Role: {currentUser.role}</p>
+          <p>Organization: {currentUser.organisation}</p>
+          <p>Email: {currentUser.mail}</p>
+        </div>
+      }
+    </>
+  );
 };
