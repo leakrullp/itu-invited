8e4015dff5c84708cccac5c9da716226a5d02ae8
leakrullp <leakrull96@gmail.com>
2025-11-05 14:20:07 +0100

    attempt at connecting to back4app server. didn't work.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AppRoutes } from "./Routes.jsx";
 import "./index.css";
+// **Important** to use the minified version
+import Parse from "parse";
+
+//javascriptKey is required only if you have it on server.
+Parse.initialize(
+  "YsjXEUYnz2m6YGZvzfqAtvsJFLcpge44m7kl7hDP",
+  "gzXItqBZFqkyaObjF7WW1sa0DRTlKXgnwHbESIbu"
+);
+Parse.serverURL = "https://parseapi.back4app.com/";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
