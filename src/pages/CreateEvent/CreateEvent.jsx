1cb4cbad96647cf3c5ad70ad0c5cef0773b30a6c
thole <thole@itu.dk>
2025-11-19 18:33:35 +0100

    refactor of handPostNow so logic in inside SaveEventToDB.js including hard coded data which right now will overwrite everything. Still no connection to user input

--- src/pages/CreateEvent/CreateEvent.jsx
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -6,10 +6,10 @@ import DatetimeInput from "../../components/Input/DatetimeInput.jsx";
 import DescriptionInputField from "../../components/Input/DescriptionInputField.jsx";
 import TagsInputDropdown from "../../components/Input/TagsInputDropdown.jsx";
 import SignupLink from "../../components/Input/SignupLink.jsx";
-
 import "./CreateEvent.css";
 import Parse from "parse"; // use our configured Parse instance
 import initializeAllParse from "../../parseConfig.js";
+import { SaveEventToDB } from "./SaveEventToDB";
 
 export const CreateEvent = () => {
   const [popupMessage, setPopupMessage] = useState("");
@@ -33,46 +33,13 @@ export const CreateEvent = () => {
     // Initialize Parse (if needed)
     initializeAllParse();
 
-    // Create a new Event table row
-    const Event = Parse.Object.extend("Event");
-    const newEvent = new Event();
-
-    // Values sent if nothing from input is given
-    //  ------DO NOT DELETE FOR TESTING PURPOSES -----
-    newEvent.set("isPosted", true);
-    newEvent.set("startTime", new Date("2025-12-22T19:30"));
-    newEvent.set("endTime", new Date("2025-12-22T19:30"));
-    newEvent.set("title", "sent_from_frontEnd");
-    newEvent.set("description", "descriptionaroisblob");
-    const orgID = "5z1KbHqPBC"; //is present in DB
-    const Organization = Parse.Object.extend("Organization");
-    const orgObj = new Organization();
-    orgObj.id = orgID;
-    newEvent.set("orgID", orgObj);
-    const selectedTags = ["Fun", "Python"]; //are present in DB
-    const EventTag = Parse.Object.extend("EventTag");
-    const tagQuery = new Parse.Query(EventTag);
-    tagQuery.containedIn("term", selectedTags); //points to tag based on value in "term" column
-    const tagObjects = await tagQuery.find();
-    const relation = newEvent.relation("eventTag");
-    tagObjects.forEach((tagObj) => {
-      relation.add(tagObj);
-    }); //add relation for each tag in the list given to the db
-    newEvent.set("signupLink", "https://erdetfredag.dk/");
-    const Picture = Parse.Object.extend("Picture");
-    const picObj = new Picture();
-    picObj.id = "sQrZAOqBFz"; // is present in DB
-    newEvent.set("eventPicID", picObj);
-
-    // Save to database
-    newEvent.save().then(
-      (savedObj) => {
-        console.log("Event saved with ID:", savedObj.id); //delete when done testing
-      },
-      (error) => {
-        console.error("Error saving event:", error); //todo: make a popup
-      }
-    );
+    try {
+      // Call service WITHOUT sending eventData
+      const savedObj = await SaveEventToDB();
+      console.log("Event saved with ID:", savedObj.id); //delete when done testing
+    } catch (error) {
+      console.error("Error saving event:", error); //todo: make a popup
+    }
   };
 
   return (
@@ -80,6 +47,7 @@ export const CreateEvent = () => {
       <main className="createevent-container">
         {/*Todo: change text to "Create an event for {logged in Organization}"*/}
         <h2 className="createevent-title">Create an event for ITUnderground</h2>
+
         <ThumbnailInput onThumbnailSaved={setThumbnailPicture} />
 
         <TitleInput />
