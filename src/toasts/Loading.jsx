8c126b346cb1d0e27ae3a87acf2066d1f186c654
kdre <kdre@itu.dk>
2025-12-21 16:08:31 +0100

    Making better loading visulas and setting it up on fronp.

---
+++ src/toasts/Loading.jsx
@@ -0,0 +1,12 @@
+import "./Toast.css";
+
+export default function Loading({ text = "Loading..."}) {
+  
+    return (
+      <div className="loading">
+        <div className="spinner" />
+        <p>{text}</p>
+      </div>
+    );
+  }
+  
